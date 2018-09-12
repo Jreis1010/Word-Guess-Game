5abfbbd0a82b43195a33088390d83c621838c67d
Joao Reis <Jreis1010@gmail.com>
2018-09-12 15:06:32 -0400

    updated files

---
+++ assets/javascript/games.js
@@ -0,0 +1,19 @@
+ var wins = 0;
+ var losses = 0;
+ var lives = 10;
+
+ var availableOptions = [10, 17, 7, 30, 21, 22]
+
+ var computerGuess = availableOptions 
+
+
+ document.onkeyup = function() {
+
+
+ }
+  
+ 
+
+
+
+
